853aa9de66eef1ff45c7762d3681052cc4edf862
Griffin Poole <griffinpoole5@gmail.com>
2019-12-08 18:31:17 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let y = record.find(function(r) {
+    return r.result === 'W';
+  });
+  if (y) {
+    return y.year;
+  } else {
+    return undefined;
+  }
+}
